5d467a79e3d1341a2e8092627684e8dece372ba4
Abish <abish265s@gmail.com>
2025-03-14 12:08:57 +0530

    program

---
+++ cases/Case.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Case{
+    public static void main(String[]args){
+        String name="hello,wordld";
+        System.out.print(name.toUpperCase());
+        System.out.print(name.toLowerCase());
+    }
+}
--- vowel/Vowel.java
+++ vowel/Vowel.java
@@ -1,22 +1,20 @@
 import java.util.Scanner;
-
 public class Vowel{
-    public static void main(String[] args) {
+    public static void main(String[]args){
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a string:");
         String input = scanner.nextLine();
-         int vowelCount = 0;
-        for (char ch : input.toLowerCase().toCharArray()) {
+         int vowelCount=0;
+        for (char ch:input.toLowerCase().toCharArray()){
             if (isVowel(ch)) {
-                System.out.println(ch + " is a vowel.");
+                System.out.println(ch+"is a vowel");
                 vowelCount++;
             }
         }
-
-        System.out.println("Total number of vowels: " + vowelCount);
+        System.out.println("total number of vowels:"+vowelCount);
         scanner.close();
     }
-    public static boolean isVowel(char ch) {
-        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    public static boolean isVowel(char ch){
+        return ch =='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
     }
 }
