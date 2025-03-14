f9c506a788dc680304db8e77c06f77279f43fa6b
Abish <abish265s@gmail.com>
2025-03-14 12:23:54 +0530

    program

--- add/Add.java
+++ add/Add.java
@@ -1,10 +1,10 @@
 import java.util.*;
 public class Add{
     void Add(int a,int b){
-        System.out.println("answer is"+a+b);
+        System.out.println("answer is"+(a+b));
         }
         void Add(int a,int b,int c){
-            System.out.println("answer is"+a+b+c);
+            System.out.println("answer is"+(a+b+c));
         }
         public static void main(String[]args){
             Add obj=new Add();
