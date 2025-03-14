1d7889dd6b27edf1920e75194d79ee4bb721675c
Abish <abish265s@gmail.com>
2025-03-14 10:48:13 +0530

    program

---
+++ vowel/Vowel.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Vowel{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+         int vowelCount = 0;
+        for (char ch : input.toLowerCase().toCharArray()) {
+            if (isVowel(ch)) {
+                System.out.println(ch + " is a vowel.");
+                vowelCount++;
+            }
+        }
+
+        System.out.println("Total number of vowels: " + vowelCount);
+        scanner.close();
+    }
+    public static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
