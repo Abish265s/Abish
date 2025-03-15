bdb75b64c830c22fed02196b0646ab455bf1ed00
Abish <abish265s@gmail.com>
2025-03-15 10:32:01 +0530

    program

---
+++ split/Split.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Split {
+    public static void main(String[] args) {
+        String name = "hello,World";
+        String[] names = name.split(","); 
+        System.out.println(names[0]);
+    }
+}
