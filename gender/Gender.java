3e77f27792f911fff6f4ff7ba2864f03a7ba76ea
Abish <abish265s@gmail.com>
2025-03-15 11:36:40 +0530

    program

---
+++ gender/Gender.java
@@ -0,0 +1,11 @@
+public class Gender {
+    int a;
+    public static void main(String[] args) {
+        String a = "male";
+        if (a.equals("male")) {
+            System.out.println("This is male");
+        } else {
+            System.out.println("This is female");
+        }
+    }
+}
